fec4bc477a50b44e1cb49cc6096fe1e91ea0907d
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2022-04-22 22:20:22 -0500

    Mobile design added

--- src/components/SearchCategory.jsx
+++ src/components/SearchCategory.jsx
@@ -11,19 +11,20 @@ const SearchCategory = ({ category, setCategory }) => {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		setCategory(inputValue);
+		setInputValue('');
 	};
 
 	return (
 		<section
 			aria-label="Search category"
-			className="flex justify-between items-center mb-6"
+			className="flex flex-col-reverse md:flex-row gap-6 md:gap-0 justify-between items-center mb-6"
 		>
 			<h2 className="uppercase text-2xl font-bold">
 				Results of <span className="text-cyan-400">{category}</span>:
 			</h2>
-			<form className="flex" onSubmit={handleSubmit}>
+			<form className="flex w-full md:w-auto" onSubmit={handleSubmit}>
 				<input
-					className="outline-none border-2 bg-neutral-900 border-neutral-700 border-r-0 focus:bg-neutral-700 p-1.5 rounded-tl-md rounded-bl-md"
+					className="outline-none border-2 bg-neutral-900 border-neutral-700 border-r-0 focus:bg-neutral-700 p-1.5 rounded-tl-md rounded-bl-md flex-1"
 					type="text"
 					value={inputValue}
 					onChange={handleInputChange}
